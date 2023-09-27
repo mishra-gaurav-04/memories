e23e9880e5942ca0533c8e93239b597efecda05e
Gaurav Mishra <gaurav.info217@gmail.com>
2023-09-27 20:48:54 +0530

    added post controller

---
+++ server/controllers/postController.js
@@ -0,0 +1,9 @@
+const Posts = require('../models/Posts');
+
+exports.getAllPosts = async(req,res,next) => {
+
+}
+
+exports.createNewPost = async(req,res,next) => {
+
+}
--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const postController = require('../controllers/postController');
 
 const router = express.Router();
+
+router.get('/',postController.getAllPosts);
+router.post('/',postController.createNewPost);
 
 module.exports = router;
